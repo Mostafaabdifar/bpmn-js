168cadd890623fbc764f82cf7ebbce1f39323cef
Mostafa <mostafaabdi1998@gmail.com>
2025-09-29 10:43:10 +0330

    add dialog for remove channel

--- src/app/components/landing/landing.ts
+++ src/app/components/landing/landing.ts
@@ -13,14 +13,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { ActivatedRoute, Router } from '@angular/router';
 import {
   ChannelClient,
   ChannelDto,
   CreateChannelCommand,
 } from '../../proxy/Integration';
-import { ActivatedRoute, Router } from '@angular/router';
-import { CoreService } from '../../service/core.service';
-import { C } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-landing',
@@ -41,16 +39,18 @@ import { C } from '@angular/cdk/keycodes';
 })
 export class Landing implements OnInit {
   @ViewChild('dialogTemplate') dialogTemplate!: TemplateRef<any>;
+  @ViewChild('removeTemplate') removeTemplate!: TemplateRef<any>;
   private currentDialogRef?: MatDialogRef<any>;
   channelList: ChannelDto[] = [];
   channelName: string = '';
   channelCommand = new CreateChannelCommand();
   deleteChannelCommand = new CreateChannelCommand();
+  selectedChannel!: ChannelDto;
+
   constructor(
     private channelClient: ChannelClient,
     private dialog: MatDialog,
     private router: Router,
-    private coreService: CoreService,
     private activatedRoute: ActivatedRoute
   ) {}
 
@@ -66,6 +66,10 @@ export class Landing implements OnInit {
 
   openDialog(): void {
     this.currentDialogRef = this.dialog.open(this.dialogTemplate);
+  }
+
+  openRemoveDialog(): void {
+    this.currentDialogRef = this.dialog.open(this.removeTemplate);
   }
 
   createChannel() {
@@ -86,11 +90,28 @@ export class Landing implements OnInit {
     });
   }
 
+  deleteChannel() {
+    console.log(this.selectedChannel);
+    this.channelClient.delete(this.deleteChannelCommand).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.getList();
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {
+        this.currentDialogRef?.close();
+      },
+    });
+  }
+
   openBpmn(channel: ChannelDto) {
     this.router.navigate(['/bpmn', channel.id]);
   }
 
   onDeleteAction(channel: ChannelDto) {
-    console.log('Delete', channel);
+    this.selectedChannel = channel;
+    this.openRemoveDialog();
   }
 }
